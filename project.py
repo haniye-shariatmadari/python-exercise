2fe05977c4a8fabe66013e04135233df2ea62aeb
haniye sh <haniye.shariatmadari@gmail.com>
2022-02-22 09:50:56 +0330

    first commit1

---
+++ project.py
@@ -0,0 +1,46 @@
+# zoj va fard
+
+# print("enter your number:")
+
+# x=int(input())
+# if (x%2 )== 0:
+
+    # print("zoje")
+# else:
+    # print("fard")
+# 
+
+
+# # x=2
+# y=4
+# z=6
+# print(max(x, y, z))
+
+
+#  تشخیص اعداد بزرگتر
+# print(" enter number 1")
+# x=int (input())
+# print(" enter number 2")
+# y=int (input())
+# print(" enter number 3")
+# z=int (input())
+
+# print(max(x,y,z))
+
+
+
+n = int(input("enter a number:"))
+for i in range (n):
+    for j in range (i):
+        print (" ", end = " ")
+    for j in range (i,n):
+        print ("*", end = " ")
+    print()
+
+
+
+n = int(input("enter a number:"))
+for i in range (n):
+    for j in range (i,n):
+        print ("*", end = " ")
+    print()
